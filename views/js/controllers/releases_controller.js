2bf52a06fdb534da129b517143f52895c7a24b40
Josh <bluemazaro@yahoo.com>
2015-12-16 23:39:43 -0800

    added functional 'save list to disk' button, currently outputs JSON, list is already sorted

--- views/js/controllers/releases_controller.js
+++ views/js/controllers/releases_controller.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.controller('releasesController', ['$scope', '$http', function($scope, $http) {
     $scope.releases = [];
 
-    $scope.testText = "Casting magick";
+    // CRUD Functions
 
     $scope.getAll = function() {
       $http({
@@ -27,6 +27,8 @@ module.exports = function(app) {
       })
       .success(function(data) {
         $scope.releases.push(data);
+        var submissions = angular.element( document.querySelector( '#submissions' ) );
+        submissions.append(release.year + ' ' + release.artistName + ' - ' + release.albumName + ' [' + release.review + '] <br/>');
       })
       .error(function(data) {
         console.log(data);
@@ -47,28 +49,6 @@ module.exports = function(app) {
       })
     };
 
-    $scope.editToggle = function(release) {
-      if(release.editing) {
-        release.albumName = release.oldAlbumName;
-        release.artistName = release.oldArtistName;
-        release.editing = false;
-      } else {
-        release.oldAlbumName = release.albumName;
-        release.oldArtistName = release.artistName;
-        release.editing = true;
-      }
-    };
-
-    $scope.expandToggle = function(release) {
-      if(release.expanded) {
-
-        release.expanded = false;
-      } else {
-
-        release.expanded = true;
-      }
-    };
-
     $scope.remove = function(release) {
       $http({
         method: 'DELETE',
@@ -81,6 +61,64 @@ module.exports = function(app) {
       .error(function(data) {
         console.log(data);
       });
+    };
+
+    // Non-CRUD functions
+
+    $scope.editToggle = function(release) {
+      if(release.editing) {
+        release.albumName = release.oldAlbumName;
+        release.artistName = release.oldArtistName;
+        release.editing = false;
+      } else {
+        release.oldAlbumName = release.albumName;
+        release.oldArtistName = release.artistName;
+        release.editing = true;
+      }
+    };
+
+    $scope.outputSubmissionstoHTML = function() {
+      console.log($scope.releases);
+    };
+
+    $scope.expandToggle = function(release) {
+      if(release.expanded) {
+
+        release.expanded = false;
+      } else {
+
+        release.expanded = true;
+      }
+    };
+
+    $scope.saveToPC = function (data, filename) {
+
+      var today = new Date();
+      var yyyy = today.getFullYear();
+      var mm = today.getMonth()+1;
+      var dd = today.getDate();
+
+      filename = yyyy + "-" + mm + "-" + dd + " Psylist.json";
+
+      if (!data) {
+        console.error('No data');
+        return;
+      }
+
+      if (typeof data === 'object') {
+        data = JSON.stringify(data, undefined, 2);
+      }
+
+      var blob = new Blob([data], {type: 'text/json'}),
+        e = document.createEvent('MouseEvents'),
+        a = document.createElement('a');
+
+      a.download = filename;
+      a.href = window.URL.createObjectURL(blob);
+      a.dataset.downloadurl = ['text/json', a.download, a.href].join(':');
+      e.initMouseEvent('click', true, false, window,
+          0, 0, 0, 0, 0, false, false, false, false, 0, null);
+      a.dispatchEvent(e);
     };
 
   }]);
